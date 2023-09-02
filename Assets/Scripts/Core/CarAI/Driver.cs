a1f67b1173024f8e0ca504dc0c0afe877b14ba8a
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-09-02 23:48:40 +0300

    init driver class

--- Assets/Scripts/Core/CarAI/Driver.cs
+++ Assets/Scripts/Core/CarAI/Driver.cs
@@ -1,7 +1,22 @@
+using UnityEngine;
+
 namespace Core.CarAI
 {
     public class Driver
     {
+        private readonly TargetFollow _targetFollow;
 
+        public float Acceleration { get; private set; }
+
+        public float Brake { get; private set; }
+
+        public float TurnAmount { get; private set; }
+
+        public float SteerSpeed { get; private set; }
+
+        public Driver(Transform carBody) 
+        {
+            _targetFollow = new TargetFollow(carBody);
+        }
     }
 }
