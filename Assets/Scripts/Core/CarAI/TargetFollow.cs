20149be7cbb39ea39350a5a667ff3732d9c7cd6d
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-09-03 15:54:15 +0300

    nu, rename

--- Assets/Scripts/Core/CarAI/TargetFollow.cs
+++ Assets/Scripts/Core/CarAI/TargetFollow.cs
@@ -4,7 +4,7 @@ namespace Core.CarAI
 {
     public class TargetFollow
     {
-        private readonly Transform _carBody;
+        private readonly Transform _carTransform;
 
         private Transform _target;
 
@@ -19,23 +19,23 @@ namespace Core.CarAI
 
         public TargetFollow(Transform carBody)
         {
-            _carBody = carBody;
+            _carTransform = carBody;
         }
 
         public void Update()
         {
-            var distance = Vector3.Distance(_carBody.position, _target.position);
+            var distance = Vector3.Distance(_carTransform.position, _target.position);
 
             var forwardAmount = 0.0f;
             var turnAmount = 0.0f;
 
             if (distance > _reachedDistance)
             {
-                var directionToMovePosition = (_target.position - _carBody.position).normalized;
-                var dot = Vector3.Dot(_carBody.forward, directionToMovePosition);
+                var directionToMovePosition = (_target.position - _carTransform.position).normalized;
+                var dot = Vector3.Dot(_carTransform.forward, directionToMovePosition);
 
                 var angleToDirection =
-                    Vector3.SignedAngle(_carBody.forward, directionToMovePosition, Vector3.up);
+                    Vector3.SignedAngle(_carTransform.forward, directionToMovePosition, Vector3.up);
 
                 forwardAmount = dot > 0 || !UseReverse ? 1.0f : -1.0f;
                 turnAmount = Mathf.Clamp(angleToDirection / _maxAngle, -1.0f, 1.0f);
