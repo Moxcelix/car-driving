4f8f91f042379f3e8d9e3e4b0569c917fe9cbb34
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-09-09 11:08:50 +0300

    implement GetAllNodes method

--- Assets/Scripts/Core/CarAI/Navigation/Path/PathFinder.cs
+++ Assets/Scripts/Core/CarAI/Navigation/Path/PathFinder.cs
@@ -1,13 +1,49 @@
 using Core.CarAI.Navigation;
+using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace Core.CarAI
 {
     public class PathFinder
     {
+        public void CreatePath(Node startNode, Node endNode)
+        {
+            var queue = new List<Node>();
+
+
+
+
+        }
+
         public float GetWeight(Node a, Node b)
         {
             return Vector3.Distance(a.transform.position, b.transform.position);
+        }
+
+        private List<Node> GetAllNodes(Node node)
+        {
+            var nodes = new List<Node>();
+            var queue = new List<Node>
+            {
+                node
+            };
+
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Last();
+
+                nodes.Add(currentNode);
+                queue.AddRange(
+                    from toQueue
+                    in currentNode.Nodes
+                    where !nodes.Contains(toQueue)
+                    select toQueue);
+
+                queue.Remove(currentNode);
+            }
+
+            return nodes;
         }
     }
 }
