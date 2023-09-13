f3439711fdd00a5c5f39d4a5df0fefa3866c998d
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-09-13 22:24:46 +0300

    funny collapse

--- Assets/Scripts/Core/CarAI/Navigation/Path/PathFinder.cs
+++ Assets/Scripts/Core/CarAI/Navigation/Path/PathFinder.cs
@@ -19,64 +19,10 @@ namespace Core.CarAI
         /// <returns></returns>
         public float CreatePath(Node startNode, Node endNode)
         {
-            var s = new List<Node>() { startNode };
+            RecalculateMarks(startNode);
+            RecalculateMinInputNodes();
 
-            _nodes = GetAllNodes(startNode);
-            _marks = new Dictionary<Node, float>() { { startNode, 0.0f } };
-            _minInputNode = new Dictionary<Node, Node>();
-
-            while (s.Count < _nodes.Count)
-            {
-                var currentNode = s.Last();
-                var minNode = (Node)null;
-
-                foreach (var node in _nodes)
-                {
-                    if (s.Contains(node))
-                    {
-                        continue;
-                    }
-
-                    var weight = GetWeight(currentNode, node);
-
-                    _marks[node] = _marks.ContainsKey(node) ?
-                        Mathf.Min(_marks[node], weight + _marks[currentNode]) :
-                        weight + _marks[currentNode];
-
-                    Debug.Log($"{currentNode}, {node} = {_marks[node]}");
-
-                    if (minNode == null ||
-                        _marks[node] < _marks[currentNode] + GetWeight(currentNode, minNode))
-                    {
-                        minNode = node;
-                    }
-                }
-
-                s.Add(minNode);
-            }
-
-            foreach(var node in _nodes)
-            {
-                foreach (var nextNode in node.Nodes)
-                {
-                    if (!_minInputNode.ContainsKey(nextNode) ||
-                        _marks[node] < _marks[_minInputNode[nextNode]])
-                    {
-                        _minInputNode[nextNode] = node;
-
-                        Debug.Log(node);
-                    }
-                }
-            }
-
-            var path = new List<Node>();
-
-            for (var node = endNode; node != startNode; node = _minInputNode[node])
-            {
-                path.Insert(0, node);
-            }
-
-            path.Insert(0, startNode);
+            var path = MakeMinPath(startNode, endNode);
 
             Debug.Log(path.Count);
 
