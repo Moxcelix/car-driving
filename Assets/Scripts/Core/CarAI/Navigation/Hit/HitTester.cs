49e21de0e50fe978bc1d5a535827b6dfb5664f65
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-09-15 12:04:49 +0300

    implement get hit

--- Assets/Scripts/Core/CarAI/Navigation/Hit/HitTester.cs
+++ Assets/Scripts/Core/CarAI/Navigation/Hit/HitTester.cs
@@ -6,11 +6,33 @@ namespace Core.CarAI.Navigation
     {
         private readonly Transform _transform;
         private readonly Vector3 _hitRay;
+        private readonly float _rayLength;
 
-        public HitTester(Transform transform, Vector3 hitRay)
+        public HitTester(Transform transform, Vector3 hitRay, float rayLength)
         {
             this._transform = transform;
             this._hitRay = hitRay;
+            this._rayLength = rayLength;
+        }
+
+        public float GetHit<T>()
+        {
+            if (Physics.Raycast(_transform.position,
+                _hitRay, out RaycastHit hit, _rayLength))
+            {
+                var tempMonoArray = hit.collider.
+                    gameObject.GetComponents<MonoBehaviour>();
+
+                foreach (var monoBehaviour in tempMonoArray)
+                {
+                    if (monoBehaviour is T)
+                    {
+                        return hit.distance;
+                    }
+                }
+            }
+
+            return float.PositiveInfinity;
         }
     }
 }
