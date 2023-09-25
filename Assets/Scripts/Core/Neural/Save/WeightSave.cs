62a3f17056abbc723f21cc7c2d39d6af534922aa
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-09-25 15:26:32 +0300

    add WeightSave

---
+++ Assets/Scripts/Core/Neural/Save/WeightSave.cs
@@ -0,0 +1,34 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Core.Neural
+{
+    public class WeightSave : IWeightProvider
+    {
+        private float[,] _weights;
+        public double GetWeight(int i, int j)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Load(string path)
+        {
+            var stream = new FileStream(path, FileMode.Open);
+            var binaryFormatter = new BinaryFormatter();
+
+            _weights = (float[,])binaryFormatter.Deserialize(stream);
+
+            stream.Close();
+        }
+
+        public void Save(string path, NeuralNetwork neuralNetwork)
+        {
+            var stream = new FileStream(path, FileMode.OpenOrCreate);
+            var binaryFormatter = new BinaryFormatter();
+
+            binaryFormatter.Serialize(stream, _weights);
+
+            stream.Close();
+        }
+    }
+}
