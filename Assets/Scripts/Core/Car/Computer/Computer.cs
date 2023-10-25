014b3d5b1ace8ddbbe8faa33af42d9b6d1e6f2ab
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-10-25 22:57:53 +0300

    computer was implemented

--- Assets/Scripts/Core/Car/Computer/Computer.cs
+++ Assets/Scripts/Core/Car/Computer/Computer.cs
@@ -2,6 +2,19 @@ namespace Core.Car
 {
     public class Computer
     {
+        private readonly Car _car;
 
+        public float Speed { get; private set; }
+
+        public TransmissionMode TransmissionMode { get; private set; }
+
+        public int Gear { get; private set; }
+
+        public void Update()
+        {
+            Speed = (int)_car.GetSpeed();
+            TransmissionMode = _car.Transmission.Mode;
+            Gear = _car.Transmission.CurrentGear;
+        }
     }
 }
