47b13f8ce1a89137db07a25e9a7d19c7ed88d5eb
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-11-16 22:23:30 +0300

    Update CentralLocking.cs

--- Assets/Scripts/Core/Car/Systems/CentralLocking.cs
+++ Assets/Scripts/Core/Car/Systems/CentralLocking.cs
@@ -2,6 +2,22 @@ namespace Core.Car
 {
     public class CentralLocking
     {
+        private readonly Door[] _doors;
+
         public bool Locked { get; set; }
+
+        public CentralLocking(Door[] doors)
+        {
+            _doors = doors;
+        }
+
+        public void Update()
+        {
+            foreach (var door in _doors)
+            {
+                door.IsLocked = Locked;
+            }
+        }
+
     }
 }
