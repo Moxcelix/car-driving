07da3c9860d88a085c50b5a1e4c51c8585405659
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-11-18 23:23:30 +0300

    Update Immobilizer.cs

--- Assets/Scripts/Core/Car/Systems/Immobilizer.cs
+++ Assets/Scripts/Core/Car/Systems/Immobilizer.cs
@@ -2,6 +2,13 @@ namespace Core.Car
 {
     public class Immobilizer
     {
+        private readonly Engine _engine;
+
         public bool IsActive { get; set; }
+
+        public Immobilizer (Engine engine)
+        {
+            _engine = engine;
+        }
     }
 }
