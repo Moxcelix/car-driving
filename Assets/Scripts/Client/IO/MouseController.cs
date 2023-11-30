6feb592905d31dffd31c3c0b5f9ee6e5db1c76a0
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-11-30 20:07:31 +0300

    Update MouseController.cs

--- Assets/Scripts/Client/IO/MouseController.cs
+++ Assets/Scripts/Client/IO/MouseController.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 public static class MouseController
 {
-    private static bool s_state = false;
+    private static bool state = false;
 
     public static void SetVisibility(bool state)
     {
-        s_state = state;
+        MouseController.state = state;
         Cursor.lockState = state ? CursorLockMode.None : CursorLockMode.Locked;
         Cursor.visible = state;
     }
 
     public static void SwitchState()
     {
-        SetVisibility(!s_state);
+        SetVisibility(!state);
     }
 }
