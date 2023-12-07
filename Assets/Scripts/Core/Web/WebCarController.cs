99c356ceae12ef7b50c381aaaa7432191f80f197
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-12-07 19:43:58 +0300

    start web

---
+++ Assets/Scripts/Core/Web/WebCarController.cs
@@ -0,0 +1,13 @@
+namespace Core.Web
+{
+    using Core.Car;
+    public class WebCarController
+    {
+        private readonly Car _car;
+
+        public WebCarController(Car car)
+        {
+            _car = car;
+        }
+    }
+}
