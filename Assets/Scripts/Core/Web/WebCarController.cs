562906f60f7debd98ae29212f19b6a12f1a7732e
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-12-15 22:14:33 +0300

    Update WebCarController.cs

--- Assets/Scripts/Core/Web/WebCarController.cs
+++ Assets/Scripts/Core/Web/WebCarController.cs
@@ -1,13 +1,32 @@
+using System;
+using System.Threading;
+
 namespace Core.Web
 {
     using Core.Car;
+    using UnityEngine;
+
     public class WebCarController
     {
         private readonly Car _car;
 
-        public WebCarController(Car car)
+        private readonly Thread _sendThread;
+        private readonly Thread _receiveThread;
+
+        private readonly int _sendTimeout;
+        private readonly int _receiveTimeout;
+
+        public WebCarController(Car car, int sendTimeout, int receiveTimeout)
         {
             _car = car;
+            _sendTimeout = sendTimeout;
+            _receiveTimeout = receiveTimeout;
+
+            _sendThread = new Thread(SendData);
+            _receiveThread = new Thread(ReceiveData);
+
+            _sendThread.Start();
+            _receiveThread.Start();
         }
 
         public void Update()
@@ -18,12 +37,22 @@ namespace Core.Web
 
         private void SendData()
         {
-            // Send
+            while (_sendThread.IsAlive)
+            {
+                Debug.Log("Send some data");
+
+                Thread.Sleep(_sendTimeout);
+            }
         }
 
         private void ReceiveData()
         {
-            // Receive
+            while (_receiveThread.IsAlive)
+            {
+                Debug.Log("Receive some data");
+
+                Thread.Sleep(_receiveTimeout);
+            }
         }
     }
 }
