b438c0872928cdd07ee9e3918b736a1d7971b120
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-01-03 23:38:49 +0300

    все, новый год закончился

--- Assets/Scripts/Client/IO/ClientIO.cs
+++ Assets/Scripts/Client/IO/ClientIO.cs
@@ -63,8 +63,6 @@ public class ClientIO :
 
     private float _steerDelta = 0;
 
-    // Happy new Year!
-
     public IControls.ToogleSwitchDelegate EngineSwitch { get; set; }
     public IControls.BlinkerStateSwitchDelegate BlinkerStateSwitch { get; set; }
     public IControls.TransmissionModeSwitchDelegate TransmissionModeSwitch { get; set; }
