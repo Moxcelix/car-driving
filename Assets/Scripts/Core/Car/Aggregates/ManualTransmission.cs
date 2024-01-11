6e2d3a867c43188426af155160e792437b672667
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-01-11 03:34:53 +0300

    manual without clutch

---
+++ Assets/Scripts/Core/Car/Aggregates/ManualTransmission.cs
@@ -0,0 +1,211 @@
+using UnityEngine;
+
+namespace Core.Car
+{
+    public enum ManualTransmissionMode
+    {
+        GEAR,
+        REVERSE,
+        NEUTRAL,
+    }
+
+    public class ManualTransmission : ITransmission
+    {
+        [SerializeField] private GameObject _selectorPos_N;
+        [SerializeField] private GameObject _selectorPos_NL;
+        [SerializeField] private GameObject _selectorPos_NLL;
+        [SerializeField] private GameObject _selectorPos_R;
+        [SerializeField] private GameObject _selectorPos_NR;
+        [SerializeField] private GameObject _selectorPos_1;
+        [SerializeField] private GameObject _selectorPos_2;
+        [SerializeField] private GameObject _selectorPos_3;
+        [SerializeField] private GameObject _selectorPos_4;
+        [SerializeField] private GameObject _selectorPos_5;
+        [SerializeField] private GameObject _selectorPos_6;
+
+        [SerializeField] private Gear[] _gears;
+        [SerializeField] private float _reverseGearRatio = 3.44f;
+        [SerializeField] private float _idlingRMP = 800;
+        [SerializeField] private float _lastGearRatio = 3.574f;
+        [SerializeField] private float _forcedSwitchRPM = 6000.0f;
+
+        private float _inputTorque = 0;
+        private float _inputRPM = 0;
+        private float _outputRPM = 0;
+        private int _currentGear = 0;
+
+        private Vector2Int _selectorPosition = Vector2Int.zero;
+
+        public ManualTransmissionMode Mode { get; private set; } = ManualTransmissionMode.NEUTRAL;
+
+
+        private void Update()
+        {
+            CurrentGear = _currentGear;
+
+            IsReverse = Mode == ManualTransmissionMode.REVERSE;
+
+            Load = Mode == ManualTransmissionMode.NEUTRAL? 0 : 1;
+
+            UpdateGear();
+            UpdateSelector();
+            UpdateTorque(_inputTorque, _inputRPM, _outputRPM, Time.deltaTime);
+            UpdateBrake();
+        }
+
+        public override void SwitchUp()
+        {
+            if (_selectorPosition.y < 1)
+            {
+                _selectorPosition.y += 1;
+            }
+        }
+
+        public override void SwitchDown()
+        {
+            if (_selectorPosition.y > -1 && _selectorPosition.x > -2)
+            {
+                _selectorPosition.y -= 1;
+            }
+            else if (_selectorPosition.y > 0)
+            {
+                _selectorPosition.y -= 1;
+            }
+        }
+
+        public override void SwitchLeft()
+        {
+            if (_selectorPosition.y != 0)
+            {
+                return;
+            }
+
+            if (_selectorPosition.x > -2)
+            {
+                _selectorPosition.x -= 1;
+            }
+        }
+
+        public override void SwitchRight()
+        {
+            if (_selectorPosition.y != 0)
+            {
+                return;
+            }
+
+            if (_selectorPosition.x < 1)
+            {
+                _selectorPosition.x += 1;
+            }
+        }
+
+        public override float GetRatio()
+        {
+            return Mode switch
+            {
+                ManualTransmissionMode.REVERSE =>
+                    -_reverseGearRatio * _lastGearRatio,
+                ManualTransmissionMode.GEAR =>
+                    _gears[_currentGear].Ratio * _lastGearRatio,
+                _ => 0,
+            };
+        }
+
+
+
+        public override void SetValues(float inputTorque, float inputRPM, float outputRPM)
+        {
+            _inputTorque = inputTorque;
+            _inputRPM = inputRPM;
+            _outputRPM = outputRPM;
+        }
+
+
+        private void UpdateBrake()
+        {
+
+        }
+
+        private void UpdateSelector()
+        {
+            _selectorPos_N.SetActive(_selectorPosition == new Vector2Int(0, 0));
+            _selectorPos_NL.SetActive(_selectorPosition == new Vector2Int(-1, 0));
+            _selectorPos_NLL.SetActive(_selectorPosition == new Vector2Int(-2, 0));
+            _selectorPos_R.SetActive(_selectorPosition == new Vector2Int(-2, 1));
+            _selectorPos_NR.SetActive(_selectorPosition == new Vector2Int(1, 0));
+            _selectorPos_1.SetActive(_selectorPosition == new Vector2Int(-1, 1));
+            _selectorPos_2.SetActive(_selectorPosition == new Vector2Int(-1, -1));
+            _selectorPos_3.SetActive(_selectorPosition == new Vector2Int(0, 1));
+            _selectorPos_4.SetActive(_selectorPosition == new Vector2Int(0, -1));
+            _selectorPos_5.SetActive(_selectorPosition == new Vector2Int(1, 1));
+            _selectorPos_6.SetActive(_selectorPosition == new Vector2Int(1, -1));
+        }
+
+        private void UpdateGear()
+        {
+            if (_selectorPosition.y == 0)
+            {
+                _currentGear = 0;
+                Mode = ManualTransmissionMode.NEUTRAL;
+            }
+            else if (_selectorPosition.y == 1)
+            {
+                switch (_selectorPosition.x)
+                {
+                    case -2:
+                        Mode = ManualTransmissionMode.REVERSE;
+                        _currentGear = 0;
+                        break;
+                    case -1:
+                        Mode = ManualTransmissionMode.GEAR;
+                        _currentGear = 0;
+                        break;
+                    case 0:
+                        Mode = ManualTransmissionMode.GEAR;
+                        _currentGear = 2;
+                        break;
+                    case 1:
+                        Mode = ManualTransmissionMode.GEAR;
+                        _currentGear = 4;
+                        break;
+                }
+            }
+            else
+            {
+                switch (_selectorPosition.x)
+                {
+                    case -1:
+                        Mode = ManualTransmissionMode.GEAR;
+                        _currentGear = 1;
+                        break;
+                    case 0:
+                        Mode = ManualTransmissionMode.GEAR;
+                        _currentGear = 3;
+                        break;
+                    case 1:
+                        Mode = ManualTransmissionMode.GEAR;
+                        _currentGear = 5;
+                        break;
+                }
+            }
+        }
+
+        private void UpdateTorque(float inputTorque,
+            float inputRPM, float outputRPM, float deltaTime)
+        {
+            var nativeRPM = outputRPM * GetRatio();
+
+            //_torqueConverter.Update(deltaTime);
+            //_torqueConverter.Convert(inputRPM, nativeRPM);
+
+            //Load = 1.0f - _torqueConverter.FluidTransition;
+            //Torque =
+            //    inputTorque *
+            //    GetRatio() *
+            //    _ratioShifter.Load *
+            //   _torqueConverter.GetRatio();
+
+            RPM = nativeRPM;
+        }
+    }
+}
