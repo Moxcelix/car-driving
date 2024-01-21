a9cfc7ae1d528d4a40fddebdaf8beef938ff7ec7
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-01-21 23:43:29 +0300

    Update CarMirror.cs

--- Assets/Scripts/Client/Environment/CarMirror.cs
+++ Assets/Scripts/Client/Environment/CarMirror.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 [Serializable]
 public class CarMirror
 {
-    [SerializeField] private RenderTexture _texture;
     [SerializeField] private Camera _camera;
+
+    private RenderTexture _texture;
 
     public void SetActive(bool active)
     {
+        if(_texture == null)
+        {
+            _texture = new RenderTexture(128, 128, 1);
+        }
+
         _camera.targetTexture = active ? _texture : null;
         _camera.enabled = active;
     }
