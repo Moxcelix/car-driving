4e4b1c88ee9b443037c118346374bb0a8cb43173
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-01-21 23:54:42 +0300

    update car mirror

--- Assets/Scripts/Client/Environment/CarMirror.cs
+++ Assets/Scripts/Client/Environment/CarMirror.cs
@@ -1,21 +1,27 @@
-using System;
 using UnityEngine;
 
-[Serializable]
-public class CarMirror
+[RequireComponent(typeof(MeshRenderer))]
+public class CarMirror : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
+    [SerializeField] private Material _defaultMaterial;
+    [SerializeField] private Material _reflectionMaterial;
 
+    private MeshRenderer _meshRenderer;
     private RenderTexture _texture;
+
+    private void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _texture = new RenderTexture(128, 128, 1);
+        _meshRenderer.material.mainTexture = _texture;
+    }
 
     public void SetActive(bool active)
     {
-        if(_texture == null)
-        {
-            _texture = new RenderTexture(128, 128, 1);
-        }
-
         _camera.targetTexture = active ? _texture : null;
         _camera.enabled = active;
+
+        _meshRenderer.material = active ? _reflectionMaterial : _defaultMaterial;
     }
 }
