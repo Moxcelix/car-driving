b59a7785fe0e4895752e84e8e650d652bae2423a
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-01-26 23:49:46 +0300

    add set torque

--- Assets/Scripts/Core/Car/Physics/RollBack.cs
+++ Assets/Scripts/Core/Car/Physics/RollBack.cs
@@ -2,35 +2,42 @@ using UnityEngine;
 
 namespace Core.Car
 {
-    [RequireComponent(typeof(Rigidbody))]
     public class RollBack : MonoBehaviour
     {
-        private Rigidbody _rigidbody;
 
         [SerializeField] private WheelCollider[] _wheelColliders;
 
         private readonly float _magnitude = 0.1f;
 
-        private void Awake()
-        {
-            _rigidbody = GetComponent<Rigidbody>();
-        }
 
         private void Update()
         {
-           
+            var torque = GetTorque();
+
+            if (Mathf.Abs(torque) < _magnitude)
+            {
+                
+            }
+        }
+
+        private void SetToqrque(float torque)
+        {
+            foreach(var wheel in _wheelColliders)
+            {
+                wheel.motorTorque = torque;
+            }
         }
 
         private float GetTorque()
         {
             var torque = 0.0f;
 
-            foreach(var wheel in _wheelColliders)
+            foreach (var wheel in _wheelColliders)
             {
                 torque += wheel.motorTorque;
             }
 
-            return torque / (float)_wheelColliders.Length; 
+            return torque / (float)_wheelColliders.Length;
         }
     }
 }
