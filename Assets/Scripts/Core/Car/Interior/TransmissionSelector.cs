c659c7ef4ae37cf3d027c257e67cd55f46f805ca
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-02-15 12:46:47 +0300

    add new selector class

---
+++ Assets/Scripts/Core/Car/Interior/TransmissionSelector.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Core.Car
+{
+    [System.Serializable]
+    public class SelectorPosition
+    {
+        [SerializeField] private Vector2Int _position;
+        [SerializeField] private string _literal;
+        [SerializeField] private GameObject _selectorObject;
+
+        public Vector2Int Position => _position;
+        public string Literal => _literal;
+        public GameObject SelectorObject => _selectorObject;
+    }
+
+    public class TransmissionSelector : MonoBehaviour
+    {
+        [SerializeField] private SelectorPosition[] _selectorPositions;
+
+        private Vector2Int _currentPosition = Vector2Int.zero;
+    }
+}
