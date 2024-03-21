5fe3e91204328b4e6d78cc7416356c7a21bc57b7
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-03-21 17:42:34 +0300

    no nasty sound

--- Assets/Scripts/Client/Sound/DoorSound.cs
+++ Assets/Scripts/Client/Sound/DoorSound.cs
@@ -1,42 +1,29 @@
-using System;
 using Core.Car;
-using System.Linq;
+using System;
 using UnityEngine;
 
 [Serializable]
 public class DoorSound
 {
-    [SerializeField] private AudioSource _doorSystemAudioSource;
     [SerializeField] private AudioSource _audioSource;
 
     [SerializeField] private AudioClip _openDoor;
     [SerializeField] private AudioClip _closeDoor;
     [SerializeField] private AudioClip _slamLid;
     [SerializeField] private AudioClip _gaslift;
-    [SerializeField] private AudioClip _openInfoSound;
 
     [SerializeField] private Door[] _doors;
     [SerializeField] private Door _hood;
     [SerializeField] private Door _trunk;
-
-    [SerializeField] private Controller[] _openControllers;
 
     public void Initialize()
     {
         _hood.OnStateChange += PlayLidSound;
         _trunk.OnStateChange += PlayLidSound;
 
-        _doorSystemAudioSource.clip = _openInfoSound;
-        _doorSystemAudioSource.loop = true;
-
         for (int i = 0; i < _doors.Length; i++)
         {
             _doors[i].OnStateChange += PlayDoorSound;
-        }
-
-        for (int i = 0; i < _openControllers.Length; i++)
-        {
-            _openControllers[i].OnStateChange += PlayOpenDoorSound;
         }
     }
 
@@ -48,11 +35,6 @@ public class DoorSound
         for (int i = 0; i < _doors.Length; i++)
         {
             _doors[i].OnStateChange -= PlayDoorSound;
-        }
-
-        for (int i = 0; i < _openControllers.Length; i++)
-        {
-            _openControllers[i].OnStateChange -= PlayOpenDoorSound;
         }
     }
 
@@ -87,27 +69,6 @@ public class DoorSound
                 break;
             default:
                 break;
-        }
-    }
-
-    private void PlayOpenDoorSound(bool state)
-    {
-        if (state == _doorSystemAudioSource.isPlaying)
-        {
-            return;
-        }
-
-        var controllers = from controller in _openControllers
-                          where controller.State
-                          select controller;
-
-        if (controllers.Count() > 0)
-        {
-            _doorSystemAudioSource.Play();
-        }
-        else
-        {
-            _doorSystemAudioSource.Stop();
         }
     }
 }
