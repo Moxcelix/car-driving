220e0a0ef3febe8a38e558d56f11da2e9e434dcd
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-04-06 23:55:04 +0300

    Update CarReflectionProbe.cs

--- Assets/Scripts/Client/Environment/CarReflectionProbe.cs
+++ Assets/Scripts/Client/Environment/CarReflectionProbe.cs
@@ -3,4 +3,11 @@ using UnityEngine;
 public class CarReflectionProbe : MonoBehaviour
 {
     [SerializeField] private ReflectionProbe _reflectionProbe;
+
+    public void SetActive(bool active)
+    {
+        _reflectionProbe.refreshMode = active ? 
+            UnityEngine.Rendering.ReflectionProbeRefreshMode.EveryFrame : 
+            UnityEngine.Rendering.ReflectionProbeRefreshMode.OnAwake;
+    }
 }
