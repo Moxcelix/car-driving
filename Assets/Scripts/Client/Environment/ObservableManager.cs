cb0ec8e8ffda5241eb3a13d842f86e55bab2b1b9
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-04-07 23:46:20 +0300

    Update ObservableManager.cs

--- Assets/Scripts/Client/Environment/ObservableManager.cs
+++ Assets/Scripts/Client/Environment/ObservableManager.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 public class ObservableManager : MonoBehaviour
 {
     [SerializeField] private CarMirror[] _mirrors;
+    [SerializeField] private CarReflectionProbe[] _reflectionProbe;
 
     public void SetActive(bool active)
     {
         foreach (var mirror in _mirrors)
         {
             mirror.SetActive(active);
+        }
+
+        foreach (var reflectionProbe in _reflectionProbe)
+        {  
+            reflectionProbe.SetActive(active); 
         }
     }
 }
