3f22aaf0fbc3545f28cca9e3170e12ad16f6e39c
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-04-16 18:57:21 +0300

    Update Tire.cs

--- Assets/Scripts/Core/Car/Physics/Tire.cs
+++ Assets/Scripts/Core/Car/Physics/Tire.cs
@@ -6,18 +6,34 @@ namespace Core.Car
     public class Tire : MonoBehaviour
     {
         [SerializeField] private float _maxForwardStiffness = 1.0f;
-        [SerializeField] private float _maxSideStiffness = 1.0f;
+        [SerializeField] private float _maxSidewaysStiffness = 1.0f;
+        [SerializeField] private float _maxStiffnessRPM = 500.0f;
+
+        private float _forwardStiffness;
+        private float _sidewaysStiffness;
 
         private WheelCollider _wheelCollider;
 
         private void Awake()
         {
             _wheelCollider = GetComponent<WheelCollider>();
+
+            _forwardStiffness = _wheelCollider.forwardFriction.stiffness;
+            _sidewaysStiffness = _wheelCollider.sidewaysFriction.stiffness;
         }
 
         private void FixedUpdate()
         {
-            
+            var transition = Mathf.Clamp01(_wheelCollider.rpm / _maxStiffnessRPM);
+
+            var forwardFriction = _wheelCollider.forwardFriction;
+            var sidewaysFriction = _wheelCollider.sidewaysFriction;
+
+            forwardFriction.stiffness = Mathf.Lerp(_forwardStiffness, _maxForwardStiffness, transition);
+            sidewaysFriction.stiffness = Mathf.Lerp(_sidewaysStiffness, _maxSidewaysStiffness, transition);
+
+            _wheelCollider.forwardFriction = forwardFriction;
+            _wheelCollider.sidewaysFriction = sidewaysFriction;
         }
     }
 }
